aebbaffa48a211bf258002e4860496cc0c5324e9
Ahmed Hamdi Saeed Ibrahim <135139481+AhmedHamdiSaeed@users.noreply.github.com>
2024-06-01 12:32:28 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingSpinnerComponent } from './SharedComponent/loading-spinner/loading-spinner/loading-spinner.component';
+import { FooterComponent } from './Components/Footer/footer/footer.component';
+import { HeaderComponent } from './Components/Header/header/header.component';
+import { SignupComponent } from './Components/Login/signup/signup.component';
+import { NotfoundComponent } from './Components/notFound/notfound/notfound.component';
+import { LoginComponent } from './Components/Login/login/login.component';
+import { UserprofileComponent } from './Components/user-profile/userprofile/userprofile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingSpinnerComponent,
+    FooterComponent,
+    HeaderComponent,
+    SignupComponent,
+    NotfoundComponent,
+    LoginComponent,
+    UserprofileComponent
   ],
   imports: [
     BrowserModule,
